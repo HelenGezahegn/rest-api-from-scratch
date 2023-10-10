8402c410093fe6369210398d6049daeb145467f0
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-09 18:37:03 -0600

    feat: add user schema + model + functions

---
+++ src/db/users.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  authentication: {
+    // select set to false so it's not selected by default
+    password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false }
+  }
+});
+
+const UserModel = mongoose.model("User", UserSchema);
+
+const getUsers = () => UserModel.find();
+const getUsersByEmail = (email: String) => UserModel.findOne({ email });
+const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.find({ "authentication.sessionToken": sessionToken });
+
+const getUserById = (id: String) => UserModel.findById(id);
+const createUser = (values: Record<string, any>) =>
+  new UserModel(values).save().then((user) => user.toObject());
+const deleteUserById = (id: String) => UserModel.findOneAndDelete({ _id: id });
+const updateUserById = (id: String, values: Record<string, any>) =>
+  UserModel.findByIdAndUpdate({ id, values });
+
+export {
+  UserModel,
+  getUsers,
+  getUsersByEmail,
+  getUserBySessionToken,
+  getUserById,
+  createUser,
+  deleteUserById,
+  updateUserById
+};
